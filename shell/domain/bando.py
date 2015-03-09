e7ddc3b50dd868552574cc9bb9d1f2affa405b76
rainbowbreeze <rainbowbreeze@gmx.com>
2015-03-09 12:32:33 +0100

    fixed pep8 issues

--- shell/domain/bando.py
+++ shell/domain/bando.py
@@ -32,3 +32,23 @@ class Bando:
         # or
         # return self.__dict__
 
+    """
+    Sources on compairing for equality
+     http://jcalderone.livejournal.com/32837.html
+     http://stackoverflow.com/questions/390250/elegant-ways-to-support-equivalence-equality-in-python-classes
+    """
+    def __eq__(self, other):
+        """Override the default Equals behavior"""
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __ne__(self, other):
+        """Define a non-equality test"""
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
+    def __hash__(self):
+        """Override the default hash behavior (that returns the id or the object)"""
+        return hash(tuple(sorted(self.__dict__.items())))
--- shell/tests/domain/test_bando.py
+++ shell/tests/domain/test_bando.py
@@ -3,7 +3,7 @@ __author__ = 'rainbowbreeze'
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
+class TestBando(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
 
