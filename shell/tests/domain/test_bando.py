d13ca890d22f416d99cf5cfa5da9fd2f4eac585d
rainbowbreeze <rainbowbreeze@gmx.com>
2015-03-09 12:46:12 +0100

    tests for bando object

--- shell/tests/domain/test_bando.py
+++ shell/tests/domain/test_bando.py
@@ -1,12 +1,31 @@
+
 __author__ = 'rainbowbreeze'
 
 import unittest
+import datetime
+from domain.bando import Bando
 
 
 class TestBando(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_equality(self):
+        now = datetime.datetime.now()
+        bando1 = Bando("Title1", now, "http://urlfile1.zip")
+        bando2 = Bando("Title1", now, "http://urlfile1.zip")
+        bando3 = Bando("Title2", now, "http://urlfile1.zip")
+        now2 = datetime.datetime.now()
+        bando4 = Bando("Title2", now2, "http://urlfile1.zip")
+        bando5 = Bando("Title2", now2, "http://urlfile2.zip")
 
+        self.assertEqual(bando1, bando2)
+        self.assertNotEqual(bando1, bando3)
+        self.assertNotEqual(bando2, bando3)
+        self.assertNotEqual(bando3, bando4)
+        self.assertNotEqual(bando4, bando5)
+        self.assertEqual(len(set([bando1, bando2])), 1)
+        self.assertEqual(len(set([bando1, bando3])), 2)
+        self.assertEqual(len(set([bando2, bando3])), 2)
+        self.assertEqual(len(set([bando1, bando2, bando3])), 2)
+        self.assertEqual(len(set([bando1, bando2, bando3, bando4, bando5])), 4)
 
 if __name__ == '__main__':
     unittest.main()
